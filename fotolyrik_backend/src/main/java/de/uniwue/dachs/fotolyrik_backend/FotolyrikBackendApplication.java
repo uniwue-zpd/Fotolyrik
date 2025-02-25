a9122ca2b5f9b83f690b3a6b86021918bffaa6f4
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-02-25 11:52:36 +0100

    Initial commit

---
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/FotolyrikBackendApplication.java
@@ -0,0 +1,13 @@
+package de.uniwue.dachs.fotolyrik_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotolyrikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotolyrikBackendApplication.class, args);
+	}
+
+}
