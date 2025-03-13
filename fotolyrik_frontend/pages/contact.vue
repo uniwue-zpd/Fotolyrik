2f014a90423c8ec9bb59cc3beb51ce4b9a9d75e6
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-03-13 14:44:05 +0100

    contact page updated

--- fotolyrik_frontend/pages/contact.vue
+++ fotolyrik_frontend/pages/contact.vue
@@ -3,7 +3,71 @@
 </script>
 
 <template>
-
+  <div class="columns-1 w-[800px]">
+    <div>
+      <img src= "https://unsplash.it/800/200"/>
+  </div>
+  <br>
+  <div class="grid grid-flow-row auto-rows-max">
+  <h1>Kontakt</h1>
+  <br>
+  <div>
+    <p>Lorem ipsum dolor sit amet, consetetur</p>
+    <p>sadipscing elitr, sed diam nonumy eirmod tempor</p>
+    <p>invidunt ut labore et dolore magna Lorem ipsum</p>
+    <p>dolor sit amet, consetetur sadipscing elitr, sed</p>
+    <p>diam Lorem ipsum dolor sit amet, consetetur</p>
+    <p>sadipscing elitr, sed diam.</p>
+  </div>
+    <br>
+    <div class="flex flex-row gap-4">
+      <div class="...">
+    <FormKit
+      type="text"
+      label="Name"
+      placeholder="Name"
+    />
+      </div>
+      <div class="...">
+        <FormKit
+        type="email"
+        label="Email"
+        validation="required"
+        validation-visibility="live"
+        placeholder="Email">
+        </FormKit>
+      </div>
+    </div>
+  <div class="grid grid-cols-1">
+    <div class="...">
+  <FormKit type="textarea"
+           rows="2"
+           name="message"
+           label="Nachricht"
+           placeholder="Ihre Nachricht"
+           validation-visibility="live">
+  </FormKit> <!-- Größe von Textfeld ändern?-->
+  </div>
+    <div class="...">
+      <FormKit
+        type="checkbox"
+      label="Ich habe die Datenschutzerklärung zur Kenntnis genommen."
+        name="Datenschutzerklärung"
+        validation="required"
+      > <!-- Farbe der Checkbox ändern? -->
+      </FormKit>
+      <pre wrap>{{ value }}</pre>
+    </div>
+    <br>
+    <div class="...">
+      <FormKit
+      type="button">
+        Senden
+      </FormKit>
+    </div>
+    </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
