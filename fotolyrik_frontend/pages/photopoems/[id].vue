6e02d6cac95e39e3486ced8d448f56f7e548e75b
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-03-13 16:35:39 +0100

    IIIF Viewer settings - Disabling ssr for all pages with '/photopoems/**' path - First very basic layout for photopoems pages

--- fotolyrik_frontend/pages/photopoems/[id].vue
+++ fotolyrik_frontend/pages/photopoems/[id].vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
+import 'tify'
+import 'tify/dist/tify.css'
+import { onMounted } from "vue";
 
+onMounted(() => {
+  new Tify({
+    container: '#tify-photopoem',
+    manifestUrl: 'https://iiif.slub-dresden.de/iiif/2/358216435-19340200/manifest.json', //placeholder link
+  })
+});
 </script>
 
 <template>
-
+  <div class="flex flex-col gap-2">
+    <h1 class="text-3xl font-bold outfit-headline">Fotogedicht</h1>
+    <div id="tify-photopoem" class="h-[500px]"/>
+  </div>
 </template>
 
 <style scoped>
