31e408c720783ed6f90e89d9a4e2c7b1c1a95933
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-03-14 10:16:08 +0100

    Backend. Implementing Base Entity with predefined columns - Abstract class "BaseEntity" allows attaching predefined columns to each table

---
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/model/BaseEntity.java
@@ -0,0 +1,37 @@
+package de.uniwue.dachs.fotolyrik_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @LastModifiedBy
+    protected String lastModifiedBy;
+}
