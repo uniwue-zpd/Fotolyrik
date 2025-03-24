a07a553c15b27e55600bb10d38a71803da2971c0
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-03-24 10:08:10 +0100

    Backend. docker-compose & API - Longer waiting times while performing database recreation - PUT and DELETE mappings for entities of type "Place"

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/controller/PlaceController.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/controller/PlaceController.java
@@ -3,6 +3,7 @@ package de.uniwue.dachs.fotolyrik_backend.controller;
 import de.uniwue.dachs.fotolyrik_backend.model.Place;
 import de.uniwue.dachs.fotolyrik_backend.repository.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,5 +25,29 @@ public class PlaceController {
     @PostMapping
     public Place addPlace(@RequestBody Place place) {
         return placeRepository.save(place);
+    }
+
+    @PutMapping("/{id}")
+    public Place updatePlace(@PathVariable Long id, @RequestBody Place place) {
+        Place existingPlace = placeRepository.findById(id).orElse(null);
+        if (existingPlace == null) {
+            return null;
+        }
+        existingPlace.setName(place.getName());
+        existingPlace.setDescription(place.getDescription());
+        existingPlace.setLatitude(place.getLatitude());
+        existingPlace.setLongitude(place.getLongitude());
+
+        return placeRepository.save(existingPlace);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePlace(@PathVariable Long id) {
+        if (placeRepository.existsById(id)) {
+            placeRepository.deleteById(id);
+            return ResponseEntity.ok("Place with id '" + id + "' deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
