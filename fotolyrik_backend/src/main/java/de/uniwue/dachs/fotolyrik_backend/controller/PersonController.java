a78a325a5ee3ec9b45a867cef6bc6a66f250dba0
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-03-24 15:08:04 +0100

    Backend. Enabling JPA auditing

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/controller/PersonController.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/controller/PersonController.java
@@ -3,6 +3,7 @@ package de.uniwue.dachs.fotolyrik_backend.controller;
 import de.uniwue.dachs.fotolyrik_backend.model.Person;
 import de.uniwue.dachs.fotolyrik_backend.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,5 +25,31 @@ public class PersonController {
     @PostMapping
     public Person addPerson(@RequestBody Person person) {
         return this.personRepository.save(person);
+    }
+
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable Long id, @RequestBody Person person) {
+        Person existingPerson = personRepository.findById(id).orElse(null);
+        if (existingPerson == null) {
+            return null;
+        }
+        existingPerson.setFirstName(person.getFirstName());
+        existingPerson.setLastName(person.getLastName());
+        existingPerson.setBirthYear(person.getBirthYear());
+        existingPerson.setDeathYear(person.getDeathYear());
+        existingPerson.setPseudonym(person.getPseudonym());
+        existingPerson.setSex(person.getSex());
+
+        return personRepository.save(existingPerson);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletePerson(@PathVariable Long id) {
+        if (personRepository.existsById(id)) {
+            personRepository.deleteById(id);
+            return ResponseEntity.ok("Person with id '" + id + "' deleted successfully");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
@@ -2,5 +2,7 @@ package de.uniwue.dachs.fotolyrik_backend.repository;
 
 import de.uniwue.dachs.fotolyrik_backend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
