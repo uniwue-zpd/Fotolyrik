c9da22b4262e78e2354bcb4efb901a0b53443d1d
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-04-09 12:08:25 +0200

    created impressum-page

--- fotolyrik_frontend/pages/impressum.vue
+++ fotolyrik_frontend/pages/impressum.vue
@@ -4,6 +4,26 @@
 
 <template>
 
+<h1 class="text-3xl outfit-headline font-bold">Impressum</h1>
+    <br>
+  <div>
+    <p>Julius-Maximilians-Universität Würzburg</p>
+    <p>Sanderring 2</p>
+    <p>97070 Würzburg</p>
+  </div>
+  <br>
+  <div>
+    <h2 class="text-1xl outfit-headline font-bold">Kontakt</h2>
+    <p>Telefon: (09 31) 31-0</p>
+    <p>Telefax: (09 31) 31-82600</p>
+    <p>E-Mail: <a class="text-blue-600/100 dark:text-sky-400/100" href="mailto:email@uni-wuerzburg.de">email@uni-wuerzburg.de</a></p>
+  </div>
+  <br>
+  <div>
+  <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
+  <p>Zuständige Aufsichtsbehörde: Lorem ipsum dolor sit amet</p>
+  </div>
+  <br> <!-- Ständiger Abstand zum Footer? -->
 </template>
 
 <style scoped>
