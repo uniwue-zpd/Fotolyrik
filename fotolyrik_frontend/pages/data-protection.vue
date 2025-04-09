51e9768d931afbf86d035fcd4ae9952cd7056ca3
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-04-09 12:20:00 +0200

    created data-protection-page

--- fotolyrik_frontend/pages/data-protection.vue
+++ fotolyrik_frontend/pages/data-protection.vue
@@ -3,7 +3,24 @@
 </script>
 
 <template>
-
+<h1 class="text-3xl outfit-headline font-bold">Datenschutzerklärung</h1>
+  <br>
+  <div>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+    <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore</p>
+    <p>magna aliquyam erat, sed diam voluptua.</p>
+    <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+  <br>
+  <div>
+    <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
+    <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+    <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
+    <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+  </div>
 </template>
 
 <style scoped>
