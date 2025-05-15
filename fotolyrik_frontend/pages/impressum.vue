47d14266c0a08b572614d4a1d3b8fa9e48f21d64
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-05-15 17:50:03 +0200

    adjusted for simpler layout

--- fotolyrik_frontend/pages/impressum.vue
+++ fotolyrik_frontend/pages/impressum.vue
@@ -4,31 +4,36 @@
 
 <template>
 <div class="mb-[0.5%]">
-<h1 class="text-3xl outfit-headline font-bold">Impressum</h1>
-    <br>
-  <div>
-    <p>Julius-Maximilians-Universität Würzburg</p>
-    <p>Sanderring 2</p>
-    <p>97070 Würzburg</p>
+  <h1 class="text-3xl outfit-headline font-bold">Impressum</h1>
+    <div class="mt-3">
+      Julius-Maximilians-Universität Würzburg <br>
+      Sanderring 2 <br>
+      97070 Würzburg
+    </div>
+  <div class="mt-5">
+    <h2 class="text-1xl outfit-headline font-bold mb-1">Kontakt</h2>
+    <div>
+    Telefon: (09 31) 31-0 <br>
+    Telefax: (09 31) 31-82600 <br>
+    E-Mail: <a class="text-[#0073C9]" href="mailto:email@uni-wuerzburg.de">email@uni-wuerzburg.de</a>
+    </div>
   </div>
-  <br>
-  <div>
-    <h2 class="text-1xl outfit-headline font-bold">Kontakt</h2>
-    <p>Telefon: (09 31) 31-0</p>
-    <p>Telefax: (09 31) 31-82600</p>
-    <p>E-Mail: <a class="text-blue-600/100 dark:text-sky-400/100" href="mailto:email@uni-wuerzburg.de">email@uni-wuerzburg.de</a></p>
-  </div>
-  <br>
-  <div>
-  <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
-  <p>Zuständige Aufsichtsbehörde: Lorem ipsum dolor sit amet</p>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
-      <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,</p>
-      <p>sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, </p>
-     <p> no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, </p>
-      <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-      <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, </p>
-      <p>no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+  <div class="mt-5">
+  <h2 class="text-1xl outfit-headline font-bold mb-1">Weiteres</h2>
+  <p>Zuständige Aufsichtsbehörde:</p>
+  <p>
+    Lorem ipsum dolor sit amet
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+    no sea takimata sanctus est Lorem ipsum dolor sit amet.
+    </p>
   </div>
 </div>
 </template>
