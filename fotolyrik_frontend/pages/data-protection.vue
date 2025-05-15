8d3fdd59ef4734fa803f506a44ec03490c25fbf6
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-05-15 18:13:49 +0200

    adjusted for simpler layout

--- fotolyrik_frontend/pages/data-protection.vue
+++ fotolyrik_frontend/pages/data-protection.vue
@@ -5,22 +5,24 @@
 <template>
 <div class="mb-[0.5%]">
 <h1 class="text-3xl outfit-headline font-bold">Datenschutzerklärung</h1>
-  <br>
-  <div>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
-    <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore</p>
-    <p>magna aliquyam erat, sed diam voluptua.</p>
-    <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
+  <div class="mt-3">
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+    sed diam nonumy eirmod tempor invidunt ut labore et dolore
+    magna aliquyam erat, sed diam voluptua.
+    At vero eos et accusam et justo duo dolores et ea rebum.</p>
     </div>
-  <br>
-  <div>
-    <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
-    <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
-    <p>sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </p>
-    <p>At vero eos et accusam et justo duo dolores et ea rebum.</p>
-    <p>Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</p>
+  <div class="mt-5">
+    <h2 class="text-1xl outfit-headline font-bold mb-1">Weiteres</h2>
+    <p>
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+      sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+    </p>
+    <p>
+      At vero eos et accusam et justo duo dolores et ea rebum.
+      Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr.
+    </p>
   </div>
 </div>
 </template>
