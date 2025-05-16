eeae58e25c1e79a311bde7de317d1c9ac1ef3f66
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-05-16 09:26:24 +0200

    Frontend. Minor improvements

--- fotolyrik_frontend/pages/impressum.vue
+++ fotolyrik_frontend/pages/impressum.vue
@@ -3,39 +3,42 @@
 </script>
 
 <template>
-<div class="mb-[0.5%]">
-  <h1 class="text-3xl outfit-headline font-bold">Impressum</h1>
-    <div class="mt-3">
-      Julius-Maximilians-Universität Würzburg <br>
-      Sanderring 2 <br>
-      97070 Würzburg
+  <div class="flex flex-col gap-2">
+    <h1 class="text-3xl outfit-headline font-bold">Impressum</h1>
+    <div class="flex flex-col">
+      <div>Julius-Maximilians-Universität Würzburg</div>
+      <div>Sanderring 2</div>
+      <div>97070 Würzburg</div>
     </div>
-  <div class="mt-5">
-    <h2 class="text-1xl outfit-headline font-bold mb-1">Kontakt</h2>
-    <div>
-    Telefon: (09 31) 31-0 <br>
-    Telefax: (09 31) 31-82600 <br>
-    E-Mail: <a class="text-[#0073C9]" href="mailto:email@uni-wuerzburg.de">email@uni-wuerzburg.de</a>
+    <div class="flex flex-col gap-2">
+      <h2 class="text-1xl outfit-headline font-bold">Kontakt</h2>
+      <div class="flex flex-col">
+        <div>Telefon: (09 31) 31-0</div>
+        <div>Telefax: (09 31) 31-82600</div>
+        <div>
+          E-Mail:
+          <a class="text-[#0073C9]" href="mailto:email@uni-wuerzburg.de">email@uni-wuerzburg.de</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col gap-2">
+      <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
+      <p>Zuständige Aufsichtsbehörde:</p>
+      <p>
+        Lorem ipsum dolor sit amet
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </p>
     </div>
   </div>
-  <div class="mt-5">
-  <h2 class="text-1xl outfit-headline font-bold mb-1">Weiteres</h2>
-  <p>Zuständige Aufsichtsbehörde:</p>
-  <p>
-    Lorem ipsum dolor sit amet
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet.
-  </p>
-  <p>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-    no sea takimata sanctus est Lorem ipsum dolor sit amet.
-    </p>
-  </div>
-</div>
 </template>
 
 <style scoped>
