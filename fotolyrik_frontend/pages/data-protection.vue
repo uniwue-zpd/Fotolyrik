df358a636980823abe6f2f8505065ed113e855de
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-05-19 14:54:46 +0200

    minor changes

--- fotolyrik_frontend/pages/data-protection.vue
+++ fotolyrik_frontend/pages/data-protection.vue
@@ -3,16 +3,16 @@
 </script>
 
 <template>
-<div class="mb-[0.5%]">
+<div class="flex flex-col gap-2">
 <h1 class="text-3xl outfit-headline font-bold">Datenschutzerklärung</h1>
-  <div class="mt-3">
+  <div class="flex flex-col">
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
     sed diam nonumy eirmod tempor invidunt ut labore et dolore
     magna aliquyam erat, sed diam voluptua.
     At vero eos et accusam et justo duo dolores et ea rebum.</p>
     </div>
-  <div class="mt-5">
-    <h2 class="text-1xl outfit-headline font-bold mb-1">Weiteres</h2>
+  <div class="flex-flex-col gap-2">
+    <h2 class="text-1xl outfit-headline font-bold">Weiteres</h2>
     <p>
       Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
       Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
