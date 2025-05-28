c547f95226c49e6e453022fcacd2fe37df0cc3c0
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-05-28 13:55:00 +0200

    Frontend. Add navigation placeholders for person pages

--- fotolyrik_frontend/pages/persons/[id].vue
+++ fotolyrik_frontend/pages/persons/[id].vue
@@ -22,48 +22,50 @@ onMounted(async () => {
 </script>
 
 <template>
-  <Card>
-    <template #title>
-      <div class="flex flex-row justify-between">
-        <h1 class="text-3xl font-bold text-[#063D79] outfit-headline">{{ person_item.first_name }} {{ person_item.last_name }}</h1>
-        <PageToolbar
-          :page_url="`http://fotolyrik.de${router.fullPath}`"
-          :api_url="`http://localhost:8080/persons/${person_id}`"
-        />
+  <div class="flex flex-col gap-2">
+    <Card>
+      <template #title>
+        <div class="flex flex-row justify-between">
+          <h1 class="text-3xl font-bold text-[#063D79] outfit-headline">{{ person_item.first_name }} {{ person_item.last_name }}</h1>
+          <PageToolbar
+              :page_url="`http://fotolyrik.de${router.fullPath}`"
+              :api_url="`http://localhost:8080/persons/${person_id}`"
+          />
+        </div>
+      </template>
+      <template #content>
+        <table class="min-w-full divide-y divide-gray-200 roboto-plain">
+          <tbody class="bg-white divide-y divide-gray-200">
+          <tr v-if="person_item.birth_year">
+            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Geburtsjahr</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.birth_year }}</td>
+          </tr>
+          <tr v-if="person_item.death_year">
+            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Sterbejahr</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.death_year }}</td>
+          </tr>
+          <tr v-if="person_item.pseudonym">
+            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Pseudonym</td>
+            <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.pseudonym }}</td>
+          </tr>
+          <tr v-if="person_item.sex">
+            <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Geschlecht</td>
+            <td v-if="person_item.sex === 'MALE'" class="px-6 py-4 whitespace-nowrap text-sm">Männlich</td>
+            <td v-else class="px-6 py-4 whitespace-nowrap text-sm">Weiblich</td>
+          </tr>
+          </tbody>
+        </table>
+      </template>
+    </Card>
+    <div class="flex flex-row justify-between">
+      <div class="flex flex-row space-x-2">
+        <i class="pi pi-arrow-left"/>
+        <div class="leading-none">Vorheriger Eintrag</div>
       </div>
-    </template>
-    <template #content>
-      <table class="min-w-full divide-y divide-gray-200 roboto-plain">
-        <tbody class="bg-white divide-y divide-gray-200">
-        <tr v-if="person_item.birth_year">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Geburtsjahr</td>
-          <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.birth_year }}</td>
-        </tr>
-        <tr v-if="person_item.death_year">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Sterbejahr</td>
-          <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.death_year }}</td>
-        </tr>
-        <tr v-if="person_item.pseudonym">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Pseudonym</td>
-          <td class="px-6 py-4 whitespace-nowrap text-sm">{{ person_item.pseudonym }}</td>
-        </tr>
-        <tr v-if="person_item.sex">
-          <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">Geschlecht</td>
-          <td v-if="person_item.sex === 'MALE'" class="px-6 py-4 whitespace-nowrap text-sm">Männlich</td>
-          <td v-else class="px-6 py-4 whitespace-nowrap text-sm">Weiblich</td>
-        </tr>
-        </tbody>
-      </table>
-    </template>
-  </Card>
+      <div class="flex flex-row space-x-2">
+        <div class="leading-none">Nächster Eintrag</div>
+        <i class="pi pi-arrow-right"/>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style>
-.speeddialbutton > .p-button {
-  background: #063D79;
-}
-
-.speeddialbutton > .p-button:hover {
-  background: rgba(6, 61, 121, 0.8);
-}
-</style>
