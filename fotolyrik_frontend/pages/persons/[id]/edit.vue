c1fa63916692d6535322118283fc9ad8235bf511
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-06-03 15:19:13 +0200

    Frontend. Embed the form for editing person data in the corresponding component

--- fotolyrik_frontend/pages/persons/[id]/edit.vue
+++ fotolyrik_frontend/pages/persons/[id]/edit.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { Person } from "~/utils/types";
+import apiClient from "~/service/api";
+import PersonForm from "~/components/forms/PersonForm.vue";
 
+const route = useRoute();
+const id = route.params.id;
+const person_item = ref<Person>({} as Person);
+
+onMounted(async () => {
+  try {
+    const response = await apiClient.get(`persons/${id}`);
+    person_item.value = response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 </script>
 
 <template>
-
+  <PersonForm
+      action="edit"
+      header="Person-Objekt bearbeiten"
+      :person="person_item ? person_item : undefined"
+  />
 </template>
 
 <style scoped>
