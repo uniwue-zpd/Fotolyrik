ccff640ab808c224fb84b2bf990141134cd112c2
Miriam Kutzera <miriam.kutzera@gmail.com>
2025-06-17 18:38:44 +0200

    bar chart example added

--- fotolyrik_frontend/pages/index.vue
+++ fotolyrik_frontend/pages/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 // TODO: Define functions to parse data for the carousel and the bar chart
 
-import { ref } from 'vue'
+import Chart from 'primevue/chart';
+
+import { ref, onMounted } from 'vue';
 
 const images = ref([
     'k-1.jpg',
@@ -51,6 +53,82 @@ const responsiveOptions = ref([
     numScroll: 1
   }
 ]);
+
+// start for bar chart example
+
+onMounted(() => {
+  chartData.value = setChartData();
+  chartOptions.value = setChartOptions();
+});
+
+const chartData = ref();
+const chartOptions = ref();
+
+const setChartData = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+
+  return {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+        borderColor: documentStyle.getPropertyValue('--p-cyan-500'),
+        data: [65, 59, 80, 81, 56, 55, 40]
+      },
+      {
+        label: 'My Second dataset',
+        backgroundColor: documentStyle.getPropertyValue('--p-gray-500'),
+        borderColor: documentStyle.getPropertyValue('--p-gray-500'),
+        data: [28, 48, 40, 19, 86, 27, 90]
+      }
+    ]
+  };
+};
+const setChartOptions = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--p-text-color');
+  const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+  const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+  return {
+    indexAxis: 'y',
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      legend: {
+        labels: {
+          color: textColor
+        }
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: textColorSecondary,
+          font: {
+            weight: 500
+          }
+        },
+        grid: {
+          display: false,
+          drawBorder: false
+        }
+      },
+      y: {
+        ticks: {
+          color: textColorSecondary
+        },
+        grid: {
+          color: surfaceBorder,
+          drawBorder: false
+        }
+      }
+    }
+  };
+};
+
+// end of bar chart example
 </script>
 
 <template>
@@ -128,7 +206,7 @@ const responsiveOptions = ref([
       <h2 class="text-2xl averia-layout italic text-[#063D79]">Fotolyrik in Zahlen</h2>
     </div>
     <div>
-
+      <Chart type="bar" :data="chartData" :options="chartOptions" class="h-[30rem]" />
     </div>
     <div class="analytics px-[3.75rem]">
       <div class="flex flex-col gap-2">
