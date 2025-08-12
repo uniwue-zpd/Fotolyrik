ef9eec6ee22ea8303950ec6cbda6a44192f9d604
Janik Haitz <jahtz.dev@proton.me>
2025-08-12 17:09:12 +0200

    replace css with custom primevue theme (#26)

--- fotolyrik_frontend/nuxt.config.ts
+++ fotolyrik_frontend/nuxt.config.ts
@@ -1,6 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import Aura from '@primevue/themes/aura';
 import tailwindcss from "@tailwindcss/vite";
+import { definePreset } from "@primevue/themes";
+
+const UniwuePreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}',
+        }
+    }
+})
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -27,7 +46,7 @@ export default defineNuxtConfig({
   primevue: {
     options: {
       theme: {
-        preset: Aura
+        preset: UniwuePreset
       }
     },
     autoImport: true
