89664205aad162b211c79c5e968a60c8d8bcaf88
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-09-30 10:46:18 +0200

    Frontend. The search dialog gets bigger padding and can be closed by clicking on the background around the dialog window (#30)

--- fotolyrik_frontend/components/UI/GeneralSearch.vue
+++ fotolyrik_frontend/components/UI/GeneralSearch.vue
@@ -59,7 +59,7 @@ const clearResults = () => {
   <button @click="visible = true">
     <i class="pi pi-search text-white"/>
   </button>
-  <Dialog v-model:visible="visible" modal position="top" class="min-w-[30%]" @hide="clearResults">
+  <Dialog v-model:visible="visible" modal :dismissableMask="true" position="top" class="min-w-[30%] p-2" @hide="clearResults">
     <template #header>
       <h2 class="text-xl font-bold outfit-headline text-[#063D79]">Suchen</h2>
     </template>
