bfa1cb3486c1ef0e1f321a23cbf9a0cef7d03e87
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-11-06 14:01:07 +0100

    Backend. Update DTO projections of PubMedium model

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/DTO/PubMediumDTO.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/DTO/PubMediumDTO.java
@@ -1,11 +1,32 @@
 package de.uniwue.dachs.fotolyrik_backend.DTO;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import de.uniwue.dachs.fotolyrik_backend.model.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.Year;
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
-public class PubMediumDTO {
-    private Long id;
+public class PubMediumDTO extends BaseEntity {
     private String title;
+    private String subtitle;
+    private Set<PlaceDTO> publicationPlaces = new HashSet<>();
+    private PublisherDTO publisher;
+    private Set<PublicationRhythmDTO> pubRhythms = new HashSet<>();
+    private String editorialOffice;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy")
+    private Year startYear;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy")
+    private Year endYear;
+
+    private Long amountVolumes;
+    private Long amountIssues;
+    private String zdbId;
+    private String notes;
 }
