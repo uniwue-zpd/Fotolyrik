13e367c8479cfe65c741a0ba5a388446db8c18f1
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-11-25 13:01:52 +0100

    Frontend. Update FullTextForm and search functionality in order to use the corresponding Pinia store (#26)

--- fotolyrik_frontend/components/forms/FullTextForm.vue
+++ fotolyrik_frontend/components/forms/FullTextForm.vue
@@ -3,16 +3,13 @@ import type { FullText } from "~/utils/types";
 import { useToast } from "primevue/usetoast";
 import { zodResolver } from "@primevue/forms/resolvers/zod";
 import { z } from "zod";
-import apiClient from "~/service/api";
 
 const toast = useToast();
 const photopoemStore = usePhotopoemStore();
+const fullTextStore = useFullTextStore();
+const photopoems = computed(() => photopoemStore.photopoems.map(p => ({ id: p.id, title: p.title, altTitle: p.altTitle })));
 
 const photopoemLoading = ref(false);
-
-onMounted(() => {
-  photopoemStore.fetchPhotopoems();
-});
 
 const props = defineProps<{
   action: "create" | "edit" | "edit-by-photopoem";
@@ -23,7 +20,11 @@ const props = defineProps<{
 const resolver = ref(
   zodResolver(
     z.object({
-      photopoem: z.object({}, {message: "Wählen Sie ein Fotogedicht aus."}),
+      photopoem: z.object({
+        id: z.number().min(1),
+        title: z.string().optional().nullable(),
+        altTitle: z.string().optional().nullable(),
+      }),
       fullText: z.string("Geben Sie einen Volltext ein."),
     })
   )
@@ -32,26 +33,26 @@ const resolver = ref(
 const onPhotopoemReload = async () => {
   if (!photopoemLoading.value) {
     photopoemLoading.value = true;
-    await photopoemStore.fetchPhotopoems(true);
+    await photopoemStore.refreshPhotopoemsData();
     photopoemLoading.value = false;
   }
 };
 
 const onFormSubmit = async (e: any) => {
   if (e.valid) {
+    console.log("Submitting form with values:", e.values);
     try {
       if (props.action === "create") {
-        await apiClient.post("/fulltexts", e.values);
+        await fullTextStore.createFullText(e.values);
         toast.add({severity: "success", summary: "Erfolg", detail: "Erfolgreich erstellt", life: 3000});
         e.reset();
       } else if (props.action === "edit" && props.fulltext?.id) {
-        await apiClient.put(`/fulltexts/${props.fulltext.id}`, e.values);
+        await fullTextStore.updateFullText(props.fulltext.id, e.values);
         toast.add({severity: "success", summary: "Erfolg", detail: "Erfolgreich upgedated", life: 3000});
       }
     } catch (error) {
       console.log(error);
       toast.add({severity: "error", summary: "Fehler", detail: "Fehler beim Senden der Nachricht", life: 3000});
-      navigateTo(`/fulltexts/${props.fulltext?.id}`);
     }
   }
 };
@@ -65,48 +66,44 @@ const onFormSubmit = async (e: any) => {
     </p>
 
     <div class="flex flex-col gap-2 border-2 border-solid rounded-md p-5 bg-none">
-      <Form 
-        v-slot="$form" 
+      <Form
+        v-slot="$form"
         class="flex flex-col gap-4"
-        :resolver 
+        :resolver
         :initialValues="props.fulltext ? props.fulltext : {}"
         :key="props.fulltext ? props.fulltext.id : 'new'"
-        @submit="onFormSubmit" 
+        @submit="onFormSubmit"
       >
-        <!-- Photopoem select -->
         <FormField v-slot="$field" name="photopoem" class="flex flex-col gap-1 flex-1">
           <label for="photopoem" class="font-bold">Fotogedicht</label>
           <div class="flex flex-row gap-4 flex-nowrap">
             <IconField class="flex-1 min-w-0">
               <InputIcon class="pi pi-book"/>
-              <Select 
-                labelId="photopoem" 
+              <Select
+                labelId="photopoem"
                 placeholder="Photogedicht auswählen"
-                class="pl-7" 
-                optionLabel="label"
-                optionValue="value"
-                :options="photopoemStore.photopoems.map(p => ({ label: `${p.title}`, value: p }))"
-                :key="photopoemStore.photopoems.length"
+                class="pl-7"
+                :optionLabel="(opt) => opt.title ? opt.title : opt.altTitle"
+                :options="photopoems"
+                :key="photopoems.length"
                 fluid
               />
             </IconField>
-            <Button 
-              icon="pi pi-refresh" 
-              severity="secondary" 
-              aria-label="Reload" 
-              :loading="photopoemLoading" 
+            <Button
+              icon="pi pi-refresh"
+              severity="secondary"
+              aria-label="Reload"
+              :loading="photopoemLoading"
               @click="onPhotopoemReload"
             />
             <NuxtLink to="/photopoems/create" target="_blank">
               <Button icon="pi pi-plus" severity="secondary" aria-label="Add" />
             </NuxtLink>
-          </div> 
+          </div>
           <Message v-if="$form.photopoem?.invalid" severity="error" size="small" variant="simple" class="flex-auto">
             {{ $form.photopoem.error.message }}
           </Message>
         </FormField>
-        
-        <!-- Fulltext field -->
         <FormField v-slot="$field" name="fullText" class="flex flex-col gap-1">
           <label for="fullText" class="font-bold">Volltext</label>
           <Textarea
@@ -120,17 +117,12 @@ const onFormSubmit = async (e: any) => {
             {{ $form.fullText.error.message }}
           </Message>
         </FormField>
-
-        <!-- Submit button -->
         <Button type="submit" severity="primary">
           {{ (props.action === "create") ? "Erstellen" : "Bearbeiten" }}
         </Button>
-
-        <!--
         <Fieldset legend="Form States" class="h-80 overflow-auto">
           <pre class="whitespace-pre-wrap">{{ $form }}</pre>
         </Fieldset>
-        -->
       </Form>
     </div>
   </div>
