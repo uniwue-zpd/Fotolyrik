a57c0bb592060da1fe808e9f16e8189e1d071a66
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-11-27 15:25:36 +0100

    Frontend. Create the form to create and edit GND-based keywords (#26)

---
+++ fotolyrik_frontend/pages/keywords/create.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+import KeywordForm from "~/components/forms/KeywordForm.vue";
+</script>
+
+<template>
+  <KeywordForm action="create" header="Neuen Schlagwort erstellen"/>
+</template>
+
+<style scoped>
+
+</style>
