1e3e9ec981909cfc0186329fb216b7e3a99e0b05
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-12-11 12:35:54 +0100

    Frontend. Add placeholder components to visualize loading process or any errors

---
+++ fotolyrik_frontend/components/UI/placeholders/SkeletonPlaceholder.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="h-10 bg-gray-300 rounded animate-pulse w-1/4"/>
+    <div class="flex flex-row space-x-5">
+      <div class="w-1/2 bg-gray-300 rounded animate-pulse"/>
+      <div class="flex flex-col gap-1 w-1/2">
+        <div class="h-10 bg-gray-300 rounded animate-pulse"/>
+        <div class="h-10 bg-gray-300 rounded animate-pulse"/>
+        <div class="h-10 bg-gray-300 rounded animate-pulse"/>
+        <div class="h-10 bg-gray-300 rounded animate-pulse"/>
+        <div class="h-10 bg-gray-300 rounded animate-pulse"/>
+      </div>
+    </div>
+    <div class="h-10 bg-gray-300 rounded animate-pulse w-1/4"/>
+    <div class="h-40 bg-gray-300 rounded animate-pulse"/>
+    <div class="h-10 bg-gray-300 rounded animate-pulse w-1/4"/>
+    <div class="flex flex-row gap-2">
+      <div class="h-40 bg-gray-300 rounded animate-pulse w-1/2"/>
+      <div class="h-40 bg-gray-300 rounded animate-pulse w-1/2"/>
+    </div>
+    <div class="h-10 bg-gray-300 rounded animate-pulse w-1/4"/>
+    <div class="flex flex-row gap-2">
+      <div class="h-32 bg-gray-300 rounded animate-pulse w-1/5"/>
+      <div class="h-32 bg-gray-300 rounded animate-pulse w-1/5"/>
+      <div class="h-32 bg-gray-300 rounded animate-pulse w-1/5"/>
+      <div class="h-32 bg-gray-300 rounded animate-pulse w-1/5"/>
+      <div class="h-32 bg-gray-300 rounded animate-pulse w-1/5"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
