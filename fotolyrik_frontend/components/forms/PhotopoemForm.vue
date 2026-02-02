b3903e09001f2c7fc7397a62a975b80d3de580d1
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2026-02-02 14:48:58 +0100

    field seitenzahl to umfang

--- fotolyrik_frontend/components/forms/PhotopoemForm.vue
+++ fotolyrik_frontend/components/forms/PhotopoemForm.vue
@@ -200,7 +200,7 @@ const onFormSubmit = async (e: any) => {
             </IconField>
           </FormField>
           <FormField v-slot="$field" name="pageCount" class="flex flex-col gap-1 w-full">
-            <label for="pageCount" class="font-bold">Seitenzahl</label>
+            <label for="pageCount" class="font-bold">Umfang</label>
             <IconField>
               <InputIcon class="pi pi-pen-to-square" />
               <InputNumber
