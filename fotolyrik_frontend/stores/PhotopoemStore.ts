527516663c4b2ef887088492bf53eb1b0faed07b
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-03-06 10:31:02 +0100

    Frontend. Update return type for 'previous' and 'next' photopoem functions to PhotoPoemDTO (#59)

--- fotolyrik_frontend/stores/PhotopoemStore.ts
+++ fotolyrik_frontend/stores/PhotopoemStore.ts
@@ -116,7 +116,7 @@ export const usePhotopoemStore = defineStore('photopoem', () => {
     function previousPhotopoem() {
         const currentIndex = photopoems.value.findIndex(p => p.id === currentPhotopoem.value?.id);
         if (currentIndex !== -1 && currentIndex) {
-            return photopoems.value[currentIndex - 1] as PhotoPoem;
+            return photopoems.value[currentIndex - 1] as PhotoPoemDTO;
         } else {
             return null;
         }
@@ -126,7 +126,7 @@ export const usePhotopoemStore = defineStore('photopoem', () => {
     function nextPhotopem() {
         const currentIndex = photopoems.value.findIndex(p => p.id === currentPhotopoem.value?.id);
         if (currentIndex !== -1 && currentIndex < photopoems.value.length - 1) {
-            return photopoems.value[currentIndex + 1] as PhotoPoem;
+            return photopoems.value[currentIndex + 1] as PhotoPoemDTO;
         } else {
             return null;
         }
