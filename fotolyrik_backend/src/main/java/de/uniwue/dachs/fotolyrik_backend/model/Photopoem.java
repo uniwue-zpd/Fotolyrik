a901f3e1d51bb7f58567a8940970dc45811de996
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2026-03-19 15:19:28 +0100

    backend. clear set and readd instead of overwriting with new set #46

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/model/Photopoem.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/model/Photopoem.java
@@ -59,7 +59,7 @@ public class Photopoem extends BaseEntity {
     )
     private Set<Person> otherContributors = new HashSet<>();
 
-    @OneToMany(mappedBy = "workContributedTo", cascade = CascadeType.ALL, orphanRemoval = false)//orphanRemoval=true TODO
+    @OneToMany(mappedBy = "workContributedTo", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Contribution> contributions = new HashSet<>();
 
     @ManyToMany
@@ -103,4 +103,11 @@ public class Photopoem extends BaseEntity {
             inverseJoinColumns = @JoinColumn(name = "language_id")
     )
     private Set<Language> languages = new HashSet<>();
+
+    public void updateContributions(Set<Contribution> newContributions) {
+        this.contributions.clear();
+        if (newContributions != null) {
+            this.contributions.addAll(newContributions);
+        }
+    }
 }
