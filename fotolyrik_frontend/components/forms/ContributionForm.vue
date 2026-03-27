10a6ffc2f1a6fa3ea9c6f5da2f298ca9812c15c8
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-03-27 12:33:06 +0100

    Frontend. Enhance styling within the 'ContributionForm' component (#46)

--- fotolyrik_frontend/components/forms/ContributionForm.vue
+++ fotolyrik_frontend/components/forms/ContributionForm.vue
@@ -45,36 +45,46 @@ const contributions = ref<Contribution[]>(
 );
 </script>
 <template>
-  {{console.log(contributions)}}
-<div v-if="contributions.length > 0" v-for="(contribution, index) in contributions" :key="contribution.renderId"  >
-  <div>
-    <div class="flex">
-      <Select
-          inputId="contributors"
-          placeholder="Mitwirkende Person auswählen"
-          :optionLabel="(opt) => opt.fullName ? opt.fullName : (opt.pseudonyms || []).join(', ')"
-          :optionValue="opt => ({id: opt.id, fullName: opt.fullName, studioName: opt.studioName, pseudonyms: opt.pseudonyms})"
-          :options="persons"
-          :virtual-scroller-options="{ itemSize: 50 }"
-          v-model="contribution.contributor"
-          filter fluid
-      > </Select>
-      <InputText placeholder="Pseudonym" v-model="contribution.pseudonym"></InputText>
-      <Select
-          class="w-64"
-          inputId="contributionRole"
-          placeholder="In Rolle"
-          :options="roleOptions"
-          optionLabel="label"
-          optionValue="value"
-          v-model="contribution.role"
-      ></Select>
-      <Button icon="pi pi-times" severity="secondary" aria-label="Remove"
-              @click="contributions.splice(index, 1)"/>
+  <div
+      v-if="contributions.length > 0"
+      v-for="(contribution, index) in contributions"
+      :key="contribution.renderId"
+  >
+    <div>
+      <div class="flex flex-row space-x-2 p-2 bg-surface-50 rounded-md shadow-sm">
+        <Select
+            inputId="contributors"
+            placeholder="Mitwirkende Person auswählen"
+            :optionLabel="(opt) => opt.fullName ? opt.fullName : (opt.pseudonyms || []).join(', ')"
+            :optionValue="opt => ({id: opt.id, fullName: opt.fullName, studioName: opt.studioName, pseudonyms: opt.pseudonyms})"
+            :options="persons"
+            :virtual-scroller-options="{ itemSize: 50 }"
+            v-model="contribution.contributor"
+            filter fluid
+        />
+        <InputText placeholder="Pseudonym" v-model="contribution.pseudonym"></InputText>
+        <Select
+            class="w-64"
+            inputId="contributionRole"
+            placeholder="In Rolle"
+            :options="roleOptions"
+            optionLabel="label"
+            optionValue="value"
+            v-model="contribution.role"
+        />
+        <Button icon="pi pi-times"
+                severity="secondary"
+                aria-label="Remove"
+                @click="contributions.splice(index, 1)"
+        />
+      </div>
+      <p class="text-red-500 text-center">{{contribution.errorMessage}}</p>
     </div>
-    <p class="text-red-500 text-center">{{contribution.errorMessage}}</p>
   </div>
-</div>
-<Button icon="pi pi-plus" severity="secondary" aria-label="Add" label="Mitwirkende hinzufügen"
-        @click="contributions.push(createEmptyContribution())"/>
+  <Button icon="pi pi-plus"
+          severity="secondary"
+          aria-label="Add"
+          label="Mitwirkende hinzufügen"
+          @click="contributions.push(createEmptyContribution())"
+  />
 </template>
