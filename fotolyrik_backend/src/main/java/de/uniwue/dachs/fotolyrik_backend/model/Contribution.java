6399a0cd48b1b5403cf1ac97133f7607585ab2b8
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2026-04-09 16:17:37 +0200

    backend. on delete bugfix #46

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/model/Contribution.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/model/Contribution.java
@@ -3,6 +3,8 @@ package de.uniwue.dachs.fotolyrik_backend.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
 @Table(name = "contribution")
@@ -10,6 +12,7 @@ import lombok.Setter;
 @Setter
 public class Contribution extends BaseEntity {
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Person contributor;
 
     private String pseudonym;
