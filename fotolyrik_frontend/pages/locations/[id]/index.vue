60321e80cc2adfbf775870816790e5f5e603daf2
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2026-04-10 12:28:46 +0200

    frontend. pages #65

---
+++ fotolyrik_frontend/pages/locations/[id]/index.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import PageToolbar from "~/components/UI/pagetools/PageToolbar.vue";
+import PhotopoemPreview from "~/components/UI/PhotopoemPreview.vue";
+
+const location_store = useLocationStore();
+const photopoem_store = usePhotopoemStore();
+
+const router = useRoute();
+const location_id = Number(router.params.id);
+const location_item = ref<LocationDTO | null>(null);
+
+const loading = ref(true);
+
+const is_location = ref<PhotoPoemDTO[] | []>([]);
+
+onMounted(async () => {
+  try {
+    await location_store.fetchLocationById(location_id);
+    location_item.value = location_store.current_location ?? null;
+    is_location.value = await photopoem_store.filterPhotopoems({ 'location-id': location_id });
+
+    useHead({
+      title: location_item.value ? `${location_item.value.name} - Ortsverzeichnis` : 'Nicht gefunden - Ortsverzeichnis',
+    });
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
+
+<template>
+  <Card v-if="location_item">
+    <template #title>
+      <div class="flex flex-col">
+        <div class="flex flex-row justify-between">
+          <h1 class="text-3xl font-bold text-primary outfit-headline">{{ location_item?.name }}</h1>
+          <PageToolbar
+              v-if="location_item"
+              :id="location_item.id"
+              entity_type="location"
+              :page_url="`${router.fullPath}`"
+          />
+        </div>
+        <h2 class="text-xl font-normal roboto-italic text-[#9bb1c9]">Ort</h2>
+      </div>
+    </template>
+    <template #content>
+      <div v-if="location_item.description" class="flex flex-col gap-2 py-5">
+        <div class="font-semibold roboto-plain">Beschreibung:</div>
+        <div class="roboto-plain text-justify">
+          {{ location_item.description }}
+        </div>
+      </div>
+      <Divider/>
+      <div class="flex flex-col gap-2">
+        <div v-if="is_location.length > 0" class="max-h-[30vh] flex flex-col gap-2">
+          <h2 class="text-xl font-bold text-primary outfit-headline">Fotogedichte an diesem Ort</h2>
+          <div class="overflow-y-auto pb-2">
+            <div class="flex flex-col gap-3 md:grid md:grid-cols-5">
+              <div v-for="photopoem in is_location" :key="photopoem.id">
+                <PhotopoemPreview :photopoem="photopoem"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <Divider/>
+      <div class="flex flex-col text-base">
+        <div v-if="location_item?.createdDate" class="flex flex-row space-x-2 roboto-plain">
+          <p class="font-semibold">Erstellt am:</p>
+          <p>{{ new Date(location_item.createdDate).toLocaleDateString() }}</p>
+        </div>
+        <div v-if="location_item?.lastModifiedDate" class="flex flex-row space-x-2 roboto-plain">
+          <p class="font-semibold">Zuletzt geändert am:</p>
+          <p>{{ new Date(location_item.lastModifiedDate).toLocaleDateString() }}</p>
+        </div>
+      </div>
+    </template>
+  </Card>
+  <div v-else class="p-4 bg-red-100 rounded-md flex justify-center gap-2">
+    <p class="text-red-700 font-bold roboto-plain text-3xl">Der Ort konnte nicht gefunden werden</p>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ fotolyrik_frontend/pages/locations/create.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LocationForm from "~/components/forms/LocationForm.vue";
+</script>
+
+<template>
+  <LocationForm action="create" header="Neuen Fundort erstellen"/>
+</template>
+
+<style scoped>
+
+</style>
