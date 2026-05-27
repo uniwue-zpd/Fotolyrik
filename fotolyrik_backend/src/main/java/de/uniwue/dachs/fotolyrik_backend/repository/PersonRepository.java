e2f6f9e986185e671153ec6b23e4653fbba2cacf
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-05-27 09:41:35 +0200

    Backend. Add endpoints to retrieve top themes and image motifs by author's ID (#73)

--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
@@ -1,8 +1,42 @@
 package de.uniwue.dachs.fotolyrik_backend.repository;
 
+import de.uniwue.dachs.fotolyrik_backend.DTO.visualization.KeywordCountDTO;
 import de.uniwue.dachs.fotolyrik_backend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    @Query("""
+    SELECT
+        k.value as keyword,
+        k.id as id,
+        COUNT(DISTINCT p.id) AS count
+    FROM Contribution c
+    JOIN c.workContributedTo p
+    JOIN p.themes k
+    WHERE c.contributor.id = :personId
+    GROUP BY k.id, k.value
+    ORDER BY COUNT(DISTINCT p.id) DESC
+    LIMIT :limit
+    """)
+    List<KeywordCountDTO> findTopThemesByPerson(Long personId, Long limit);
+
+    @Query("""
+    SELECT
+        k.value as keyword,
+        k.id as id,
+        COUNT(DISTINCT p.id) AS count
+    FROM Contribution c
+    JOIN c.workContributedTo p
+    JOIN p.imageMotifs k
+    WHERE c.contributor.id = :personId
+    GROUP BY k.id, k.value
+    ORDER BY COUNT(DISTINCT p.id) DESC
+    LIMIT :limit
+    """)
+    List<KeywordCountDTO> findTopImageMotifsByPerson(Long personId, Long limit);
+}
