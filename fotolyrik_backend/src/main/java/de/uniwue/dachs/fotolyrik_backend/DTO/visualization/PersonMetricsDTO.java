af6d60070a5a7fa8aea2621e2324a7661c3818a8
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-06-03 10:59:01 +0200

    Backend. Introduce 'PersonMetricsDTO' interface and repository method for in order to get descriptive statistics of a person (#78)

---
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/DTO/visualization/PersonMetricsDTO.java
@@ -0,0 +1,9 @@
+package de.uniwue.dachs.fotolyrik_backend.DTO.visualization;
+
+public interface PersonMetricsDTO {
+    Long getPhotopoems();
+    Long getPubMedia();
+    Long getKeywords();
+    Long getPhotographersWorkedWith();
+    Long getAuthorsWorkedWith();
+}
--- fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
+++ fotolyrik_backend/src/main/java/de/uniwue/dachs/fotolyrik_backend/repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package de.uniwue.dachs.fotolyrik_backend.repository;
 
 import de.uniwue.dachs.fotolyrik_backend.DTO.visualization.KeywordCountDTO;
+import de.uniwue.dachs.fotolyrik_backend.DTO.visualization.PersonMetricsDTO;
 import de.uniwue.dachs.fotolyrik_backend.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -41,4 +42,31 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     LIMIT :limit
     """)
     List<KeywordCountDTO> findTopImageMotifsByPerson(Long personId, Long limit);
+
+    @Query(value = """
+    SELECT
+        COUNT(DISTINCT p.id) AS photopoems,
+        COUNT(DISTINCT pm.id) AS pubMedia,
+        COUNT(DISTINCT k.keyword_id) AS keywords,
+        COUNT(DISTINCT phtr.contributor_id) AS photographersWorkedWith,
+        COUNT(DISTINCT authr.contributor_id) AS authorsWorkedWith
+    FROM contribution c
+    JOIN photopoem p ON c.photopoem_id = p.id
+    LEFT JOIN pub_medium pm ON p.pub_medium_id = pm.id
+    LEFT JOIN (
+        SELECT pt.photopoem_id AS photopoem_id, pt.keyword_id AS keyword_id
+        FROM photopoem_themes pt
+        UNION
+        SELECT pim.photopoem_id AS photopoem_id, pim.keyword_id AS keyword_id
+        FROM photopoem_image_motifs pim
+    ) k ON k.photopoem_id = p.id
+    LEFT JOIN contribution phtr
+        ON phtr.photopoem_id = p.id
+        AND phtr.role = 'PHOTOGRAPHER'
+    LEFT JOIN contribution authr
+        ON authr.photopoem_id = p.id
+        AND authr.role = 'AUTHOR'
+    WHERE c.contributor_id = :personId
+    """, nativeQuery = true)
+    PersonMetricsDTO getMetricsByPerson(Long personId);
 }
