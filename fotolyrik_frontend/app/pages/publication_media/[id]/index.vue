e4b6484cc01e6bc530330675343b5d613bc232e1
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-06-17 08:15:40 +0200

    Frontend. Improve responsibility of pages displaying metadata of single publication media (#84)

--- fotolyrik_frontend/app/pages/publication_media/[id]/index.vue
+++ fotolyrik_frontend/app/pages/publication_media/[id]/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 import PageToolbar from "~/components/UI/pagetools/PageToolbar.vue";
 import PhotopoemPreview from "~/components/UI/PhotopoemPreview.vue";
 import PubMediumMetrics from "~/components/visualizations/PubMediumMetrics.vue";
@@ -40,17 +39,18 @@ onMounted(async () => {
         </div>
       </template>
       <template #content>
-        <div class="flex">
-        <table class="min-w-half divide-y divide-gray-200 roboto-plain">
-          <tbody v-if="pub_medium_item" class=" divide-y divide-gray-200">
-          <tr v-if="pub_medium_item.subtitle">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Titel-Zusatz</td>
-            <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.subtitle }}</td>
-          </tr>
-          <tr v-if="pub_medium_item.publicationPlaces.length > 0">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsorte</td>
-            <td class="px-6 py-4 whitespace-nowrap ">
-              <div class="flex flex-wrap gap-3.5">
+        <div class="flex flex-col gap-4">
+          <div class="flex flex-col gap-2 md:flex-row">
+            <table class="min-w-1/3 divide-y divide-gray-200 roboto-plain">
+              <tbody v-if="pub_medium_item" class=" divide-y divide-gray-200">
+              <tr v-if="pub_medium_item.subtitle">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Titel-Zusatz</td>
+                <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.subtitle }}</td>
+              </tr>
+              <tr v-if="pub_medium_item.publicationPlaces.length > 0">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsorte</td>
+                <td class="px-6 py-4 whitespace-nowrap ">
+                  <div class="flex flex-wrap gap-3.5">
                 <span v-for="place in pub_medium_item.publicationPlaces" :key="place.id">
                   <NuxtLink
                       :to="`/places/${place.id}`"
@@ -59,50 +59,50 @@ onMounted(async () => {
                     {{ place.name }}
                   </NuxtLink>
                 </span>
-              </div>
-            </td>
-          </tr>
-          <tr v-if="pub_medium_item.publisher">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Herausgeber</td>
-            <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.publisher.name }}</td>
-          </tr>
-          <tr v-if="pub_medium_item.pubRhythms.length > 0">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsrhythmen</td>
-            <td class="px-6 py-4 whitespace-nowrap ">
-              <div class="flex flex-wrap gap-3.5">
-                <div v-for="rhythm in pub_medium_item.pubRhythms" :key="rhythm.id">
-                  <div class="p-1.5 bg-gray-accent rounded-md shadow-sm hover:shadow-md font-semibold">
-                    {{ rhythm.value }}
                   </div>
+                </td>
+              </tr>
+              <tr v-if="pub_medium_item.publisher">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Herausgeber</td>
+                <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.publisher.name }}</td>
+              </tr>
+              <tr v-if="pub_medium_item.pubRhythms.length > 0">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsrhythmen</td>
+                <td class="px-6 py-4 whitespace-nowrap ">
+                  <div class="flex flex-wrap gap-3.5">
+                    <div v-for="rhythm in pub_medium_item.pubRhythms" :key="rhythm.id">
+                      <div class="p-1.5 bg-gray-accent rounded-md shadow-sm hover:shadow-md font-semibold">
+                        {{ rhythm.value }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr v-if="pub_medium_item.startYear && pub_medium_item.endYear">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsjahre</td>
+                <td class="px-6 py-4 whitespace-nowrap ">
+                  {{ pub_medium_item.startYear }} - {{ pub_medium_item.endYear }}
+                </td>
+              </tr>
+              <tr v-if="pub_medium_item.amountVolumes">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Jahrgänge</td>
+                <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.amountVolumes }}</td>
+              </tr>
+              <tr v-if="pub_medium_item.amountIssues">
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">Ausgaben</td>
+                <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.amountIssues }}</td>
+              </tr>
+              </tbody>
+            </table>
+            <PubMediumMetrics v-if="pub_medium_metrics" :data="pub_medium_metrics" :width="500"/>
+          </div>
+          <div v-if="pub_medium_photopoems.length > 0" class="max-h-[30vh] flex flex-col gap-2">
+            <h2 class="text-xl font-bold text-primary outfit-headline">Fotogedichte in "{{ pub_medium_item?.title }}"</h2>
+            <div class="overflow-y-auto pb-2">
+              <div class="flex flex-col gap-3 md:grid md:grid-cols-5">
+                <div v-for="photopoem in pub_medium_photopoems" :key="photopoem.id">
+                  <PhotopoemPreview :photopoem="photopoem"/>
                 </div>
-              </div>
-            </td>
-          </tr>
-          <tr v-if="pub_medium_item.startYear && pub_medium_item.endYear">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Publikationsjahre</td>
-            <td class="px-6 py-4 whitespace-nowrap ">
-              {{ pub_medium_item.startYear }} - {{ pub_medium_item.endYear }}
-            </td>
-          </tr>
-          <tr v-if="pub_medium_item.amountVolumes">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Jahrgänge</td>
-            <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.amountVolumes }}</td>
-          </tr>
-          <tr v-if="pub_medium_item.amountIssues">
-            <td class="px-6 py-4 whitespace-nowrap font-semibold">Ausgaben</td>
-            <td class="px-6 py-4 whitespace-nowrap ">{{ pub_medium_item.amountIssues }}</td>
-          </tr>
-          </tbody>
-        </table>
-
-        <PubMediumMetrics v-if="pub_medium_metrics" :data="pub_medium_metrics"></PubMediumMetrics>
-        </div>
-        <div v-if="pub_medium_photopoems.length > 0" class="max-h-[30vh] flex flex-col gap-2">
-          <h2 class="text-xl font-bold text-primary outfit-headline">Fotogedichte in "{{ pub_medium_item?.title }}"</h2>
-          <div class="overflow-y-auto pb-2">
-            <div class="flex flex-col gap-3 md:grid md:grid-cols-5">
-              <div v-for="photopoem in pub_medium_photopoems" :key="photopoem.id">
-                <PhotopoemPreview :photopoem="photopoem"/>
               </div>
             </div>
           </div>
