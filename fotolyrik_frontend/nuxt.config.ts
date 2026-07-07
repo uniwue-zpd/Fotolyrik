81640cdda171a2a350e04ab92283fe0394d9858e
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2026-07-07 11:04:53 +0200

    Frontend. remove unused internal Keycloak server URL from configuration (#32)

--- fotolyrik_frontend/nuxt.config.ts
+++ fotolyrik_frontend/nuxt.config.ts
@@ -42,7 +42,6 @@ export default defineNuxtConfig({
         oauth: {
             keycloak: {
                 serverUrl: '',
-                serverUrlInternal: '',
                 realm: '',
                 clientId: '',
                 clientSecret: '',
