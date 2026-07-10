43a179e28c2862fe61f8da6f57f38021bc0b494f
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-10 09:45:04 +0200

    Frontend. Integrate 'NotAuthorizedBanner' for unauthorized access handling in 'FullTextForm' component (#32)

--- fotolyrik_frontend/app/components/forms/FullTextForm.vue
+++ fotolyrik_frontend/app/components/forms/FullTextForm.vue
@@ -3,6 +3,9 @@ import type { FullTextDTO } from "~/utils/types";
 import { useToast } from "primevue/usetoast";
 import { zodResolver } from "@primevue/forms/resolvers/zod";
 import { z } from "zod";
+import NotAuthorizedBanner from "~/components/UI/banners/NotAuthorizedBanner.vue";
+
+const { loggedIn } = useUserSession();
 
 const toast = useToast();
 const photopoemStore = usePhotopoemStore();
@@ -58,73 +61,75 @@ const onFormSubmit = async (e: any) => {
 </script>
 
 <template>
-  <div class="flex flex-col mx-auto w-[70%] gap-4">
-    <h1 class="text-2xl outfit-headline text-primary font-bold">{{ props.header }}</h1>
-    <p class="roboto-plain">
-      Füllen Sie bitte die untenstehenden Felder aus, um ein Objekt zu erstellen oder anzupassen.
-    </p>
-
-    <div class="flex flex-col gap-2 border-2 border-solid rounded-md p-5 bg-none">
-      <Form
-        v-slot="$form"
-        class="flex flex-col gap-4"
-        :resolver
-        :initialValues="props.fulltext ? props.fulltext : {}"
-        :key="props.fulltext ? props.fulltext.id : 'new'"
-        @submit="onFormSubmit"
-      >
-        <FormField v-slot="$field" name="photopoem" class="flex flex-col gap-1 flex-1">
-          <label for="photopoem" class="font-bold">Fotogedicht</label>
-          <div class="flex flex-row gap-4 flex-nowrap">
-            <IconField class="flex-1 min-w-0">
-              <InputIcon class="pi pi-book"/>
-              <Select
-                labelId="photopoem"
-                placeholder="Photogedicht auswählen"
-                class="pl-7"
-                :optionLabel="(opt) => opt.title ? opt.title : opt.altTitle"
-                :options="photopoems"
-                :key="photopoems.length"
-                fluid
+  <AuthState v-slot="{ loggedIn }">
+    <div v-if="loggedIn" class="flex flex-col mx-auto w-[70%] gap-4">
+      <h1 class="text-2xl outfit-headline text-primary font-bold">{{ props.header }}</h1>
+      <p class="roboto-plain">
+        Füllen Sie bitte die untenstehenden Felder aus, um ein Objekt zu erstellen oder anzupassen.
+      </p>
+      <div class="flex flex-col gap-2 border-2 border-solid rounded-md p-5 bg-none">
+        <Form
+            v-slot="$form"
+            class="flex flex-col gap-4"
+            :resolver
+            :initialValues="props.fulltext ? props.fulltext : {}"
+            :key="props.fulltext ? props.fulltext.id : 'new'"
+            @submit="onFormSubmit"
+        >
+          <FormField v-slot="$field" name="photopoem" class="flex flex-col gap-1 flex-1">
+            <label for="photopoem" class="font-bold">Fotogedicht</label>
+            <div class="flex flex-row gap-4 flex-nowrap">
+              <IconField class="flex-1 min-w-0">
+                <InputIcon class="pi pi-book"/>
+                <Select
+                    labelId="photopoem"
+                    placeholder="Photogedicht auswählen"
+                    class="pl-7"
+                    :optionLabel="(opt) => opt.title ? opt.title : opt.altTitle"
+                    :options="photopoems"
+                    :key="photopoems.length"
+                    fluid
+                />
+              </IconField>
+              <Button
+                  icon="pi pi-refresh"
+                  severity="secondary"
+                  aria-label="Reload"
+                  :loading="photopoemLoading"
+                  @click="onPhotopoemReload"
               />
-            </IconField>
-            <Button
-              icon="pi pi-refresh"
-              severity="secondary"
-              aria-label="Reload"
-              :loading="photopoemLoading"
-              @click="onPhotopoemReload"
+              <NuxtLink to="/photopoems/create" target="_blank">
+                <Button icon="pi pi-plus" severity="secondary" aria-label="Add" />
+              </NuxtLink>
+            </div>
+            <Message v-if="$form.photopoem?.invalid" severity="error" size="small" variant="simple" class="flex-auto">
+              {{ $form.photopoem.error.message }}
+            </Message>
+          </FormField>
+          <FormField v-slot="$field" name="fullText" class="flex flex-col gap-1">
+            <label for="fullText" class="font-bold">Volltext</label>
+            <Textarea
+                id="fullText"
+                placeholder="Die Sonne tönt nach alter Weise..."
+                rows="10"
+                autoResize
+                fluid
             />
-            <NuxtLink to="/photopoems/create" target="_blank">
-              <Button icon="pi pi-plus" severity="secondary" aria-label="Add" />
-            </NuxtLink>
-          </div>
-          <Message v-if="$form.photopoem?.invalid" severity="error" size="small" variant="simple" class="flex-auto">
-            {{ $form.photopoem.error.message }}
-          </Message>
-        </FormField>
-        <FormField v-slot="$field" name="fullText" class="flex flex-col gap-1">
-          <label for="fullText" class="font-bold">Volltext</label>
-          <Textarea
-            id="fullText"
-            placeholder="Die Sonne tönt nach alter Weise..."
-            rows="10"
-            autoResize
-            fluid
-          />
-          <Message v-if="$form.fullText?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.fullText.error.message }}
-          </Message>
-        </FormField>
-        <Button type="submit" severity="primary">
-          {{ (props.action === "create") ? "Erstellen" : "Bearbeiten" }}
-        </Button>
-        <Fieldset legend="Form States" class="h-80 overflow-auto">
-          <pre class="whitespace-pre-wrap">{{ $form }}</pre>
-        </Fieldset>
-      </Form>
+            <Message v-if="$form.fullText?.invalid" severity="error" size="small" variant="simple">
+              {{ $form.fullText.error.message }}
+            </Message>
+          </FormField>
+          <Button type="submit" severity="primary">
+            {{ (props.action === "create") ? "Erstellen" : "Bearbeiten" }}
+          </Button>
+          <Fieldset legend="Form States" class="h-80 overflow-auto">
+            <pre class="whitespace-pre-wrap">{{ $form }}</pre>
+          </Fieldset>
+        </Form>
+      </div>
     </div>
-  </div>
+    <NotAuthorizedBanner v-else/>
+  </AuthState>
 </template>
 
 <style scoped>
