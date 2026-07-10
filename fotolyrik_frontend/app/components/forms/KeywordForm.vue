21e0d26e7f6e885f08b13f5fd52110184e02a8e8
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-10 09:42:06 +0200

    Frontend. Integrate 'NotAuthorizedBanner' for unauthorized access handling in 'KeywordForm' component (#32)

--- fotolyrik_frontend/app/components/forms/KeywordForm.vue
+++ fotolyrik_frontend/app/components/forms/KeywordForm.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import {zodResolver} from "@primevue/forms/resolvers/zod";
 import {z} from "zod";
+import NotAuthorizedBanner from "~/components/UI/banners/NotAuthorizedBanner.vue";
+
+const { loggedIn } = useUserSession();
 
 const toast = useToast();
 const keywordStore = useKeywordStore();
@@ -41,61 +44,64 @@ const onFormSubmit = async (e: any) => {
 </script>
 
 <template>
-  <div class="flex flex-col mx-auto w-[70%] gap-4">
-    <h1 class="text-2xl outfit-headline text-primary font-bold">{{ props.header }}</h1>
-    <p class="roboto-plain">
-      Füllen Sie bitte die untenstehenden Felder aus, um ein Schlagwort zu erstellen oder anzupassen.
-    </p>
-    <div class="flex flex-col gap-2 border-2 border-solid rounded-md p-5 bg-none">
-      <Form
-          v-slot="$form"
-          class="flex flex-col gap-4"
-          :resolver
-          :initialValues="props.keyword ? props.keyword : {}"
-          :key="props.keyword ? props.keyword.id : 'new'"
-          @submit="onFormSubmit"
-      >
-        <FormField v-slot="$field" name="value" class="flex flex-col gap-1 flex-1">
-          <label for="value" class="font-bold">Begriff</label>
-          <IconField>
-            <InputIcon class="pi pi-user-edit" />
-            <InputText
-                id="value"
-                placeholder="Alter"
-                v-on:keydown.enter.prevent
-                fluid
-            />
-          </IconField>
-          <Message v-if="$form.value?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.value.error.message }}
-          </Message>
-        </FormField>
-        <FormField v-slot="$field" name="gndId" class="flex flex-col gap-1 flex-1">
-          <label for="gndId" class="font-bold">GND-ID</label>
-          <IconField>
-            <InputIcon class="pi pi-user-edit" />
-            <InputText
-                id="gndId"
-                placeholder="4001446-0"
-                v-on:keydown.enter.prevent
-                fluid
-            />
-          </IconField>
-          <Message v-if="$form.value?.invalid" severity="error" size="small" variant="simple">
-            {{ $form.value.error.message }}
-          </Message>
-        </FormField>
-        <Button type="submit" severity="primary">
-          {{ (props.action === "create") ? "Erstellen" : "Bearbeiten" }}
-        </Button>
-        <!--
-        <Fieldset legend="Form States" class="h-80 overflow-auto">
-          <pre class="whitespace-pre-wrap">{{ $form }}</pre>
-        </Fieldset>
-        -->
-      </Form>
+  <AuthState v-slot="{ loggedIn }">
+    <div v-if="loggedIn" class="flex flex-col mx-auto w-[70%] gap-4">
+      <h1 class="text-2xl outfit-headline text-primary font-bold">{{ props.header }}</h1>
+      <p class="roboto-plain">
+        Füllen Sie bitte die untenstehenden Felder aus, um ein Schlagwort zu erstellen oder anzupassen.
+      </p>
+      <div class="flex flex-col gap-2 border-2 border-solid rounded-md p-5 bg-none">
+        <Form
+            v-slot="$form"
+            class="flex flex-col gap-4"
+            :resolver
+            :initialValues="props.keyword ? props.keyword : {}"
+            :key="props.keyword ? props.keyword.id : 'new'"
+            @submit="onFormSubmit"
+        >
+          <FormField v-slot="$field" name="value" class="flex flex-col gap-1 flex-1">
+            <label for="value" class="font-bold">Begriff</label>
+            <IconField>
+              <InputIcon class="pi pi-user-edit" />
+              <InputText
+                  id="value"
+                  placeholder="Alter"
+                  v-on:keydown.enter.prevent
+                  fluid
+              />
+            </IconField>
+            <Message v-if="$form.value?.invalid" severity="error" size="small" variant="simple">
+              {{ $form.value.error.message }}
+            </Message>
+          </FormField>
+          <FormField v-slot="$field" name="gndId" class="flex flex-col gap-1 flex-1">
+            <label for="gndId" class="font-bold">GND-ID</label>
+            <IconField>
+              <InputIcon class="pi pi-user-edit" />
+              <InputText
+                  id="gndId"
+                  placeholder="4001446-0"
+                  v-on:keydown.enter.prevent
+                  fluid
+              />
+            </IconField>
+            <Message v-if="$form.value?.invalid" severity="error" size="small" variant="simple">
+              {{ $form.value.error.message }}
+            </Message>
+          </FormField>
+          <Button type="submit" severity="primary">
+            {{ (props.action === "create") ? "Erstellen" : "Bearbeiten" }}
+          </Button>
+          <!--
+          <Fieldset legend="Form States" class="h-80 overflow-auto">
+            <pre class="whitespace-pre-wrap">{{ $form }}</pre>
+          </Fieldset>
+          -->
+        </Form>
+      </div>
     </div>
-  </div>
+    <NotAuthorizedBanner v-else/>
+  </AuthState>
 </template>
 
 <style scoped>
